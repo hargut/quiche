5644134ab8b1a4654bb5922cace295572e1695dc
Harald Gutmann <harald@hargut.com>
2025-04-09 17:29:54 +0200

    tls: add rustls feature & tls/rustls module

--- quiche/src/tls/mod.rs
+++ quiche/src/tls/mod.rs
@@ -2,3 +2,8 @@
 mod boringssl_openssl;
 #[cfg(any(feature = "boringssl-vendored", feature = "boringssl-boring-crate", feature = "openssl"))]
 pub use boringssl_openssl::*;
+
+#[cfg(feature = "rustls")]
+mod rustls;
+#[cfg(feature = "rustls")]
+pub use rustls::*;
