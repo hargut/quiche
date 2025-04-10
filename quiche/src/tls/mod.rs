338babd00d5e74df12e7b917be55b5294337d14c
Harald Gutmann <harald@hargut.com>
2025-04-10 10:04:52 +0200

    tls: move tls::ExData to tls/mod.rs

--- quiche/src/tls/mod.rs
+++ quiche/src/tls/mod.rs
@@ -7,3 +7,24 @@ pub use boringssl_openssl::*;
 mod rustls;
 #[cfg(feature = "rustls")]
 pub use rustls::*;
+
+use crate::packet;
+use crate::ConnectionError;
+
+pub struct ExData<'a> {
+    pub application_protos: &'a Vec<Vec<u8>>,
+
+    pub pkt_num_spaces: &'a mut [packet::PktNumSpace; packet::Epoch::count()],
+
+    pub session: &'a mut Option<Vec<u8>>,
+
+    pub local_error: &'a mut Option<ConnectionError>,
+
+    pub keylog: Option<&'a mut Box<dyn std::io::Write + Send + Sync>>,
+
+    pub trace_id: &'a str,
+
+    pub recovery_config: crate::recovery::RecoveryConfig,
+
+    pub is_server: bool,
+}
