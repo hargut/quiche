45ce438f9e75e6a7b1a292d69977e1310a5c0c3a
Harald Gutmann <harald@hargut.com>
2025-04-11 11:46:58 +0200

    crypto: move crypto::Level to crypto/mod.rs

--- quiche/src/crypto/mod.rs
+++ quiche/src/crypto/mod.rs
@@ -7,3 +7,26 @@ pub use boringssl_openssl::*;
 mod rustls;
 #[cfg(feature = "rustls")]
 pub use rustls::*;
+
+use crate::packet;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Level {
+    Initial   = 0,
+    ZeroRTT   = 1,
+    Handshake = 2,
+    OneRTT    = 3,
+}
+
+impl Level {
+    pub fn from_epoch(e: packet::Epoch) -> Level {
+        match e {
+            packet::Epoch::Initial => Level::Initial,
+
+            packet::Epoch::Handshake => Level::Handshake,
+
+            packet::Epoch::Application => Level::OneRTT,
+        }
+    }
+}
