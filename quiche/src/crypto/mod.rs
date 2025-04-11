77958e1938f42c39d594379904b9163fdb68896d
Harald Gutmann <harald@hargut.com>
2025-04-11 11:46:58 +0200

    crypto: move crypto::Level to crypto/mod.rs

--- quiche/src/crypto/mod.rs
+++ quiche/src/crypto/mod.rs
@@ -2,3 +2,26 @@
 mod boringssl_openssl;
 #[cfg(any(feature = "boringssl-vendored", feature = "boringssl-boring-crate", feature = "openssl"))]
 pub use boringssl_openssl::*;
+
+use crate::packet;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Level {
+    Initial   = 0,
+    ZeroRTT   = 1,
+    Handshake = 2,
+    OneRTT    = 3,
+}
+
+impl Level {
+    pub fn from_epoch(e: packet::Epoch) -> Level {
+        match e {
+            packet::Epoch::Initial => Level::Initial,
+
+            packet::Epoch::Handshake => Level::Handshake,
+
+            packet::Epoch::Application => Level::OneRTT,
+        }
+    }
+}
