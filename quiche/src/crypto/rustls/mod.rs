556d777c0e301f3931816fb24ba56afa75a2dee0
Harald Gutmann <harald@hargut.com>
2025-04-17 16:23:24 +0200

    tls: decryption client & server tests working

--- quiche/src/crypto/rustls/mod.rs
+++ quiche/src/crypto/rustls/mod.rs
@@ -1,42 +1,111 @@
 use crate::crypto::Algorithm;
 use crate::Error;
 use crate::Result;
+use ring::aead;
+use rustls::crypto::cipher::AeadKey;
+use rustls::crypto::cipher::Iv;
+use rustls::quic;
+use rustls::quic::Algorithm as RustlsAlgorithm;
+use rustls::quic::HeaderProtectionKey;
+use rustls::quic::Keys;
+use rustls::quic::PacketKey as RustlsPacketKey;
+use rustls::quic::PacketKeySet;
+use rustls::quic::Suite;
+use rustls::quic::Tag;
+use rustls::quic::Version;
+use rustls::CipherSuite;
+use rustls::Side;
 
-pub struct PacketKey {}
+// TODO:
+//#[cfg(all(feature = "aws-lc-rs", not(feature = "ring")))]
+// use aws_lc_rs::aead;
+
+use crate::frame::Frame::Crypto;
+use ring::aead::Aad;
+use ring::aead::LessSafeKey;
+use ring::aead::Nonce;
+use ring::aead::UnboundKey;
+use ring::aead::AES_128_GCM;
+use ring::aead::MAX_TAG_LEN;
+use rustls::crypto::CryptoProvider;
+
+pub struct PacketKey {
+    key: LessSafeKey,
+    nonce: Vec<u8>,
+}
 
 #[allow(unused_variables)]
 impl PacketKey {
     pub fn new(
         alg: Algorithm, key: Vec<u8>, iv: Vec<u8>, _enc: u32,
     ) -> Result<Self> {
-        todo!()
+        let key = LessSafeKey::new(
+            UnboundKey::new(&AES_128_GCM, &key).map_err(|_| Error::CryptoFail)?,
+        );
+
+        Ok(Self { key, nonce: iv })
     }
 
     pub fn seal_with_u64_counter(
         &self, counter: u64, ad: &[u8], buf: &mut [u8], in_len: usize,
         extra_in: Option<&[u8]>,
     ) -> Result<usize> {
-        todo!()
+        let nonce = <[u8; 12]>::try_from(self.nonce.clone()).map_err(|e| {
+            error!("failed to convert nonce: {:?}", e);
+            Error::CryptoFail
+        })?;
+
+        let nonce = Nonce::assume_unique_for_key(nonce);
+        let aad = Aad::from(ad);
+
+        let tag = self
+            .key
+            .seal_in_place_separate_tag(nonce, Aad::from(ad), &mut [])
+            .map_err(|e| {
+                error!("failed to seal with packet key: {}", e);
+                Error::CryptoFail
+            })?;
+
+        buf.copy_from_slice(tag.as_ref());
+        Ok(MAX_TAG_LEN)
     }
 }
 
-pub struct Open {}
+pub struct Open {
+    packet_key: Box<dyn RustlsPacketKey>,
+    header_protection_key: Box<dyn HeaderProtectionKey>,
+    algorithm: Algorithm,
+}
 
 #[allow(unused_variables)]
 impl Open {
-    pub fn new_mask(&self, sample: &[u8]) -> Result<[u8; 5]> {
-        todo!()
+    pub fn decrypt_hdr(
+        &self, sample: &[u8], first: &mut u8, packet_number: &mut [u8],
+    ) -> Result<()> {
+        self.header_protection_key
+            .decrypt_in_place(sample, first, packet_number)
+            .map_err(|e| {
+                error!("failed to decrypt packet header: {:?}", e);
+                Error::CryptoFail
+            })
     }
 
     pub fn open_with_u64_counter(
-        &self, counter: u64, ad: &[u8], buf: &mut [u8],
+        &self, packet_number: u64, header: &[u8], payload: &mut [u8],
     ) -> Result<usize> {
-        todo!()
+        let decrypted = self
+            .packet_key
+            .decrypt_in_place(packet_number, header, payload)
+            .map_err(|e| {
+                error!("failed to decrypt packet: {:?}", e);
+                Error::CryptoFail
+            })?;
+
+        Ok(decrypted.len())
     }
 
     pub fn alg(&self) -> Algorithm {
-        // self.alg
-        todo!()
+        self.algorithm
     }
 
     pub fn derive_next_packet_key(&self) -> Result<Open> {
@@ -44,21 +113,52 @@ impl Open {
     }
 }
 
-pub struct Seal {}
+pub struct Seal {
+    packet_key: Box<dyn RustlsPacketKey>,
+    header_protection_key: Box<dyn HeaderProtectionKey>,
+    algorithm: Algorithm,
+}
 
 #[allow(unused_variables)]
 impl Seal {
     pub const ENCRYPT: u32 = 1;
 
-    pub fn new_mask(&self, sample: &[u8]) -> Result<[u8; 5]> {
-        todo!()
+    pub fn encrypt_hdr(
+        &self, sample: &[u8], first: &mut u8, packet_number: &mut [u8],
+    ) -> Result<()> {
+        self.header_protection_key
+            .encrypt_in_place(sample, first, packet_number)
+            .map_err(|e| {
+                error!("failed to encrypt packet header: {:?}", e);
+                Error::CryptoFail
+            })
     }
 
     pub fn seal_with_u64_counter(
         &self, counter: u64, ad: &[u8], buf: &mut [u8], in_len: usize,
         extra_in: Option<&[u8]>,
     ) -> Result<usize> {
-        todo!()
+
+        if (in_len + self.packet_key.tag_len()) > buf.len() {
+            error!("provided buffer size not sufficient for data and tag");
+            return Err(Error::CryptoFail)
+        }
+
+        let tag =
+            self.packet_key
+                .encrypt_in_place(counter, ad, buf)
+                .map_err(|e| {
+                    error!("failed to encrypt packet: {:?}", e);
+                    Error::CryptoFail
+                })?;
+
+        let tag_len = tag.as_ref().len();
+        let tag = tag.as_ref();
+        for ti in 0..tag_len {
+            buf[in_len + ti] = tag[ti];
+        }
+
+        Ok(in_len + tag_len)
     }
 
     pub fn alg(&self) -> Algorithm {
@@ -71,12 +171,71 @@ impl Seal {
     }
 }
 
-#[allow(unused)]
 pub fn derive_initial_key_material(
     cid: &[u8], version: u32, is_server: bool, did_reset: bool,
 ) -> Result<(Open, Seal)> {
-    let open = Open {};
-    let seal = Seal {};
+    let mut provider = CryptoProvider::get_default();
+    if provider.is_none() {
+        rustls::crypto::aws_lc_rs::default_provider()
+            .install_default()
+            .map_err(|e| {
+                error!("failed to initialize crypto provider");
+                Error::CryptoFail
+            })?;
+        provider = CryptoProvider::get_default();
+    };
+
+    let provider = provider.ok_or_else(|| {
+        error!("crypto provider not installed");
+        Error::CryptoFail
+    })?;
+
+    let suite = provider
+        .cipher_suites
+        .iter()
+        .find(|s| s.suite() == CipherSuite::TLS13_AES_128_GCM_SHA256)
+        .ok_or_else(|| {
+            error!("default crypto suite not available");
+            Error::CryptoFail
+        })?;
+
+    let tls_13_suite = suite.tls13().ok_or_else(|| {
+        error!("crypto suite not a TLS 1.3 suite");
+        Error::CryptoFail
+    })?;
+    tls_13_suite.quic_suite();
+
+    let quic_suite = tls_13_suite.quic_suite().ok_or_else(|| {
+        error!("crypto suite not a TLS 1.3 suite");
+        Error::CryptoFail
+    })?;
+
+    let side = if is_server {
+        Side::Server
+    } else {
+        Side::Client
+    };
+
+    let version = match version {
+        1 => Version::V1,
+        _ => return Err(Error::CryptoFail),
+    };
+
+    error!("side: {:?}, is_server: {:?}", side, is_server);
+
+    let keys =
+        Keys::initial(version, quic_suite.suite, quic_suite.quic, cid, side);
+
+    let open = Open {
+        packet_key: keys.remote.packet,
+        header_protection_key: keys.remote.header,
+        algorithm: Algorithm::AES128_GCM,
+    };
+    let seal = Seal {
+        packet_key: keys.local.packet,
+        header_protection_key: keys.local.header,
+        algorithm: Algorithm::AES128_GCM,
+    };
 
     Ok((open, seal))
 }
