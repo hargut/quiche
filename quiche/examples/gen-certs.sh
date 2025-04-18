f70551639c13fdc3a45c782cad8b37c663a3ae5e
Harald Gutmann <harald@hargut.com>
2025-04-18 16:36:52 +0200

    gen-certs: add subjectAltName with DNS entry to certificates
    required for successful rustls validation

--- quiche/examples/gen-certs.sh
+++ quiche/examples/gen-certs.sh
@@ -2,8 +2,9 @@
 set -ex
 cd $(dirname $0)
 openssl req -new -x509 -batch -nodes -days 10000 -keyout rootca.key -out rootca.crt
-openssl req -new -batch -nodes -sha256 -keyout cert.key -out cert.csr -subj '/C=GB/CN=quic.tech'
-openssl x509 -req -days 10000 -in cert.csr -CA rootca.crt -CAkey rootca.key -CAcreateserial -out cert.crt
+echo '[v3_req]' > openssl.cnf
+openssl req -config openssl.cnf -new -batch -nodes -sha256 -keyout cert.key -out cert.csr -subj '/C=GB/CN=quic.tech' -addext "subjectAltName=DNS:quic.tech"
+openssl x509 -req -days 10000 -in cert.csr -CA rootca.crt -CAkey rootca.key -CAcreateserial -out cert.crt -copy_extensions copy
 openssl verify -CAfile rootca.crt cert.crt
 cp cert.crt cert-big.crt
 cat cert.crt >> cert-big.crt
